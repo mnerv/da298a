f4c9190d90417ad9c26aee2dbf65d2da87b8c3ee
PettR-git <81619101+PettR-git@users.noreply.github.com>
2022-11-22 12:56:50 +0100

    tests: Address to u32 tests implemented

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,6 +1,8 @@
 /**
  * @file    tests.cpp
  * @author  Pratchaya Khansomboon (me@mononerv.dev)
+ * @author  Petter Rignell
+ * @author  Christian Heisterkamp
  * @brief   Test runner
  * @date    2022-11-18
  * 
@@ -8,7 +10,7 @@
  * 
  */
 #include "gtest/gtest.h"
-
+#include "fmt/format.h"
 #include "sky.hpp"
 
 TEST(sky_framework, make_buffer_from_mcp) {
@@ -17,13 +19,45 @@ TEST(sky_framework, make_buffer_from_mcp) {
 }
 
 TEST(sky_framework, make_mcp_from_buffer) {
-    // TODO: Test make_mcp from buffer
-    ASSERT_TRUE(false);
+    //In sky.hpp -> auto make_mcp(mcp_buffer const& src) -> mcp;
+    sky::mcp_buffer src = { 0x00, 0x04, 0x00 };
+    sky::address_t address_source = { 0x00, 0x00, 0x01 };
+    sky::mcp expected_mcp = {
+        1,
+        {0x00, 0x04, 0x00},
+        {0x00, 0x01, 0x00},
+        {0x00},
+        1
+    };
+
+    
+    mcp message{
+        1,
+        {0, 0, 255},
+        {0, 0, 2},
+        {15},
+        1
+    };
+
+    uint8_t* data = (uint8_t*)&message;
+    for (auto i = 0; i < sizeof(mcp); ++i) {
+        fmt::print("{:#04x} ", data[i]);
+    }
+    fmt::print("\n");
+
+    //sky::mcp the_mcp = 
+    //EXPECT_EQ(expected_value, output);
+    fmt::print("{}\n", the_mcp);
+
 }
 
 TEST(sky_framework, convert_address_to_u32) {
-    // TODO: Test address_t conversion to u32
-    ASSERT_TRUE(false);
+    constexpr sky::address_t address = { 0x00, 0x04, 0x00 };
+    uint32_t output = sky::address_to_u32(address);
+    uint32_t expected_value = 1024;
+    fmt::print("{}\n", output);
+
+    EXPECT_EQ(expected_value, output);
 }
 
 TEST(sky_framework, convert_u32_to_address) {
@@ -33,7 +67,10 @@ TEST(sky_framework, convert_u32_to_address) {
     constexpr sky::address_t expected_value = { 0x00, 0x04, 0x00 };
     for (size_t i = 0; i < sizeof(sky::address_t); ++i) {
         EXPECT_EQ(expected_value[i], output[i]);
+        fmt::print("{:#04x} ", output[i]);
     }
+    fmt::print("\n");
+   
 }
 
 auto main(int argc, char const* argv[]) -> int {
